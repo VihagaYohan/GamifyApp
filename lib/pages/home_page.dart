e87c7e0809d9374287921511989c46d21b4b5e55
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-01 07:34:38 +0530

    Added image to home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
 
+import '../data.dart';
+
 class HomePage extends StatefulWidget {
- @override
+  @override
   State<StatefulWidget> createState() {
     return _HomePageState();
   }
 }
 
-// private class 
+// private class
 class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
     // TODO: implement build
     return Scaffold(
-      body: Container());
+        body: Stack(
+      children: <Widget>[_featuresGAmeWidget()],
+    ));
+  }
+
+  Widget _featuresGAmeWidget() {
+    return SizedBox(
+        height: _deviceHeight * 0.5,
+        width: _deviceWidth,
+        child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+          image: NetworkImage(featuredGames[0].coverImage.url),
+        ))));
   }
 }
